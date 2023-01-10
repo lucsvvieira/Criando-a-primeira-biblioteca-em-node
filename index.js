da27a0edd8d39532748f79efc564a45c8044de5b
Lucas Vieira <vieira07lucas@gmail.com>
2023-01-10 11:24:11 -0300

    Realizando tratamento de erros

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ import fs from 'fs';
 import chalk from 'chalk';
 import { encode } from 'punycode';
 
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'Arquivo não encontrado neste diretório'));
+}
+
 function pegaArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+        if (erro) {
+            trataErro(erro);
+        }
         console.log(chalk.green(texto));
-    });
+    })
 }
 
 pegaArquivo('./arquivos/texto.md');
