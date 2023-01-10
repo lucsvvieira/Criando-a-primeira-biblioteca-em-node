c3f91327a0834beba47c7f23e7da1f77afebcc28
Lucas Vieira <vieira07lucas@gmail.com>
2023-01-10 18:00:12 -0300

    Retornando os links

--- index.js
+++ index.js
@@ -2,16 +2,13 @@ import fs from 'fs';
 import chalk from 'chalk';
 import { encode } from 'punycode';
 
-const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).';
-
 function extraiLinks(texto) {
     const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
-    const capturas = regex.exec(texto);
-    console.log(capturas);
-
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura[1]]: captura
+        [2]}));
+    return resultados;
 }
-
-extraiLinks(textoTeste);
 
 function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'Arquivo não encontrado neste diretório'));
@@ -24,7 +21,7 @@ async function pegaArquivo(caminhoDoArquivo) {
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile
         (caminhoDoArquivo, encoding)
-        console.log(chalk.green(texto));
+        console.log(extraiLinks(texto));
     } catch (erro) {
         trataErro(erro);
     } finally {
@@ -32,4 +29,4 @@ async function pegaArquivo(caminhoDoArquivo) {
     }
 }
 
-// pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/texto.md');
