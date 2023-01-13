43a4eba27fed8e9264749f17d156e3dd5aceb161
Lucas Vieira <vieira07lucas@gmail.com>
2023-01-13 13:54:47 -0300

    Gerando uma lista de URLs

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,3 +1,7 @@
+function extraiLinks(arrLinks)  {
+   return arrLinks.map((objetoLink) => Object.values(objetoLink).join())
+}
+
 export default function listaValidada(listaDeLinks) {
-    return 'Entrou na função';
+    return extraiLinks(listaDeLinks);
 }
